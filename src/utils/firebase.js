5b3389149cc98229dc8ba075019920223f758297
Ray Blandford <ray@Rays-MBP.ngohome.priv>
2020-09-11 04:57:03 -0400

    init project and create Header

---
+++ src/utils/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD8WTZwZFmBL18V5EcKsB9u-BLHXT8mqg4",
+  authDomain: "not-amzn.firebaseapp.com",
+  databaseURL: "https://not-amzn.firebaseio.com",
+  projectId: "not-amzn",
+  storageBucket: "not-amzn.appspot.com",
+  messagingSenderId: "551683448894",
+  appId: "1:551683448894:web:b33891c1ef57365164f16f",
+};
